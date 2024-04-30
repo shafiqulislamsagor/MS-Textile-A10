bf48c795b0927dcdbd32d604d7a2a67b99fad0e4
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-04-30 18:51:05 +0600

    somthing

--- src/page/MyCraftitems.jsx
+++ src/page/MyCraftitems.jsx
@@ -10,10 +10,10 @@ const MyCraftitems = () => {
 
     // console.log(user);
     useEffect(() => {
-        if(user !== null){
+        if (user !== null) {
             fetch(`https://sm-bead.vercel.app/users/${user.uid}`)
-            .then(res => res.json())
-            .then(data => setCards(data))
+                .then(res => res.json())
+                .then(data => setCards(data))
         }
     }, [user])
 
@@ -29,9 +29,9 @@ const MyCraftitems = () => {
         <div className="relative my-10 w-10/12 md:w-11/12 mx-auto overflow-x-auto shadow-md sm:rounded-lg">
             <h2 className="text-4xl font-bold text-center mb-12">My Products</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-            {
-                        cards.slice(1).map((card, id) => <Mycard key={id} cardData={card} />)
-                    }
+                {
+                    cards.slice(1).map((card, id) => <Mycard key={id} cardData={card} />)
+                }
             </div>
         </div>
     );
